aeff946df2c8ac98867b879cb6530ea9595f6971
AinazEstiri <46255442+AinazEstiri@users.noreply.github.com>
2023-04-19 16:55:28 -0700

    misplaceTile & euclideanDistance func. are done (pls check!)

--- Project1/searchAlgorthims.py
+++ Project1/searchAlgorthims.py
@@ -1,60 +1,32 @@
 
-import numpy as np
-from queue import PriorityQueue
-from TreeNodeClasses import Node, Tree
-#PLEASE ADD YIUR FILE HERE
+import GameState 
+import Search
 
-#READ!!!!!! The "__move" is the fucntion jsuggles wrote in discord, the actions the nodes can take. 
-            #please look at the code and see what edits it might need to work with your code
+#PLEASE CHECK these functions (change anything you think should be changed to match her guidelines)
 
-
-
-# "startState"/"goalState" assumed to be a matrix represented as a numpy array.
-def uniformCostSearch(startState, goalState):
-    #WRITE THE CRAP:
-
-    # useing priority queue
-    frontier = PriorityQueue()
-    startNode = Node(startState)
-    #Uniform Cost Search is just A* with h(n) hardcoded to equal zero
-    frontier.put((0, startNode)) 
-    checkedStates = set()
-
-    while not frontier.empty():
-        #gets highest prio (lowest cost) from queue, the [1] will then set startState (2nd parameter) to currentNode
-        currentNode = frontier.get()[1] 
-       
-        #if goal state is reached
-        if currentNode.state == goalState:
-            #  DO THE EXPANDING INTERFACE  HERE?!
-            while currentNode.parent is not None: #while not root node
-                __move.append(currentNode.operator) #Do the moves/actions
-                currentNode = currentNode.parent #go up a level
-            return __move[::-1]  #-1 makes sure the operator states are in order (from initial state to goal state)
-    
-        checkedStates.add(currentNode.state) #current node was checked so add to checkedStates
-
-        for continuedNode in currentNode.expand():
-
-            #only add the states that have not been seen/checked before to the queue
-            if continuedNode.state not in checkedStates:
-                #frontier.put((continuedNode.edgeCost, continuedNode)) IDK if we have an edge cost
-                frontier.put((0, continuedNode))
-
-    
-    return #goal? #do the expanding interface here????
+def misplacedTile(state):
+    misplaced = 0
+    goalState = [[1,2,3],[4,5,6],[7,8,0]]
+    for i in range(len(state)):
+        for j in range(len(state[i])):
+            if state[i][j] != goalState[i][j] and state[i][j] != 0:
+                misplaced += 1
+    return misplaced
 
 
 
 
-def MisplacedTile(matrix):
-    #write the crap
-    #do the expanding interface here
-    return #goal?
+def euclideanDistance(state):
+    distance = 0
+    goalState = [[1,2,3],[4,5,6],[7,8,0]]
+    for i in range(len(state)):
+        for j in range(len(state[i])):
+            if state[i][j] != 0:
+                tile = state[i][j]
+                goalPosition = [(numIndex, row.index(tile)) for numIndex, row in enumerate(goalState) if tile in row][0]
+                distance += ((i - goalPosition[0])**2 + (j - goalPosition[1])**2)**0.5
+    return distance
 
-def EuclideanDistance(matrix):
-    #write the crap
-    #do the expanding interface here
-    return #goal?
 
-
+#Search(startState, misplacedTile)
+#Search(startState,  euclideanDistance)
