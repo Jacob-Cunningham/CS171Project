852c46fd1d0aff1867bec6ecf6f7db5d0116f4ed
Billy Chau <bchau010@ucr.edu>
2023-06-04 17:31:12 -0700

    Main menu reformat

--- Searches.py
+++ Searches.py
@@ -8,29 +8,6 @@ def evaluate_solution(solution, data):
             correct_count += 1
     return correct_count
 
-
-def greedy_search(data): #WIP
-    bestsolution = {}
-    bestscore = 0
-    feature_count = len(data[0][1])
-
-    while True:
-        current = dict(bestsolution)  # Make a copy of the best solution
-        random_feature = random.randint(0, feature_count - 1)
-        random_data_point = random.choice(data)
-        random_features = random_data_point[1]
-        random_class_label = random_data_point[0]
-        current[tuple(random_features)] = random_class_label
-
-        currentscore = evaluate_solution(current, data)
-        if currentscore > bestscore:
-            bestsolution = dict(current)
-            bestscore = currentscore
-        else:
-            break
-
-    return bestsolution
-
 """
 Forward selection greedy search algorithm. Takes two arguments.
 num_features is the number of features each item has
